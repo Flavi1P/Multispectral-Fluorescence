83b17d89e41500e6b077293251a2323aabb84bcd
Flavi1P <flavien.petit@obs-vlfr.fr>
2019-11-18 10:40:26 +0100

    update ith new data

--- Tonga/exploration.R
+++ Tonga/exploration.R
@@ -1,10 +1,12 @@
 library(tidyverse)
 
-tonga_01 <- read_table2("Tonga/Data/TONGA_ECO1.txt")
-tonga_02 <- read_table2("Tonga/Data/TONGA_ECO2.txt")
+tonga_01 <- read_table2("Tonga/Data/TONGA_CTD_ECO1.txt")
+tonga_02 <- read_table2("Tonga/Data/TONGA_CTD_ECO2.txt")
 tonga <- bind_rows(tonga_01, tonga_02)
 
-tonga_to_plot <- filter(tonga, F440 > 0  & Station %in% c("2", "6", "7", "8"))
+
+tonga_to_plot <- filter(tonga, F440 > 0)  & Station %in% c("2", "6", "7", "8"))
+
 ggplot(tonga_to_plot)+
   geom_point(aes(x = F440, y = -Pressure, colour = "F440"))+
   geom_point(aes(x = F470, y = -Pressure, colour = "F470"))+
@@ -31,20 +33,20 @@ ggplot(tonga_to_plot)+
   scale_color_brewer(palette = "Set1")+
   ylim(-500,0)+
   theme_minimal()+
-  facet_wrap( .~ Station, nrow = 1)
+  facet_wrap( .~ Station)
 
 ggplot(tonga_to_plot)+
   geom_path(aes(x = ratio440_470, y = -Pressure), colour = "#2b8cbe", size = 0.9)+
   geom_line(aes(x = 1, y = -Pressure), colour = "Red")+
   ylim(-500,0)+
   theme_minimal()+
-  facet_wrap( .~ Station)
+  facet_wrap( .~ Station, scales = "free_x")
 
 ggplot(tonga_to_plot)+
-  geom_point(aes( x = ratio440_470, y = -Pressure, colour = Station), size = 1.5)+
-  scale_color_brewer(palette = "YlGnBu")+
+  geom_point(aes( x = ratio440_470, y = -Pressure), size = 1.5)+
+  scale_color_distiller(palette = "YlGnBu")+
   ylim(-1000, -500)+
   xlim(0.85,1.2)+
-  theme_dark()
+  theme_minimal()
 
 
