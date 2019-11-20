d94ed356f61e47bde0c8fcfd14c2c2a6d00b5804
Flavi1P <flavien.petit@obs-vlfr.fr>
2019-11-20 10:23:44 +0100

    merge the two files tonga1 and tonga2

--- Tonga/exploration.R
+++ Tonga/exploration.R
@@ -1,32 +1,40 @@
 library(tidyverse)
 
+#read data
 tonga_01 <- read_table2("Tonga/Data/TONGA_CTD_ECO1.txt")
 tonga_02 <- read_table2("Tonga/Data/TONGA_CTD_ECO2.txt")
+
+#make one file
 tonga <- bind_rows(tonga_01, tonga_02)
 
-
+#remove missing data 
 tonga_to_plot <- filter(tonga, F440 > 0)
+tonga_to_plot_base <- filter(tonga, FCHL > 0)
+
+#bin data every 5 meters
+
 
 tonga_bin <- tonga_to_plot %>%
-  mutate(bin = floor((Pressure/5) + 1)) %>% 
-  group_by(bin, Station) %>%
-  summarise_at(.vars = c("F440", "F470", "F532"), .funs = list(mean)) %>%
+  mutate(bin = floor((Pressure/5) + 1)) %>%  #create a column for the index of the bin
+  group_by(bin, Station) %>% #group the df on each bin ans station
+  summarise_at(.vars = c("F440", "F470", "F532", "Chlorophyll"), .funs = list(mean)) %>% #mean parameters in each group
   mutate(ratio440_470 = F440 / F470,
-         ratio440_532 = F440/F532) %>%
+         ratio440_532 = F440/F532) %>% #compute some ratios
   ungroup()
 
-tonga_to_plot <- tonga_to_plot %>% mutate(bin = floor((Pressure/5) + 1)) %>%
-  select(bin, Station, Pressure) %>% 
-  left_join(tonga_bin, by = c("bin", "Station"))
+tonga_bin_base <- tonga_to_plot_base %>%
+  mutate(bin = floor((Pressure/5) + 1)) %>%  #create a column for the index of the bin
+  group_by(bin, Station) %>% #group the df on each bin ans station
+  summarise_at(.vars = c("FCHL", "BB700", "PAR"), .funs = list(mean)) %>% #mean parameters in each group
+  ungroup()
 
-ggplot(tonga_to_plot)+
-  geom_point(aes(x = F440, y = -Pressure, colour = "F440"))+
-  geom_point(aes(x = F470, y = -Pressure, colour = "F470"))+
-  geom_point(aes(x = F532, y = -Pressure, colour = "F532"))+
-  scale_color_brewer(palette = "Set1")+
-  ylim(-500,0)+
-  theme_minimal()+
-  facet_wrap( .~ Station, scales = "free_x")
+tonga_to_plot <- tonga_to_plot %>% mutate(bin = floor((Pressure/5) + 1)) %>% #create an index for the bin in the other df
+  select(bin, Station, Pressure) %>% 
+  left_join(tonga_bin, by = c("bin", "Station"))#merge the two to keep all the depth values
+
+tonga_to_plot_base <- tonga_to_plot_base %>% mutate(bin = floor((Pressure/5) + 1)) %>% #create an index for the bin in the other df
+  select(bin, Station, Pressure) %>% 
+  left_join(tonga_bin_base, by = c("bin", "Station"))#merge the two to keep all the depth values
 
 ggplot(tonga_to_plot)+
   geom_path(aes(x = F440, y = -Pressure, colour = "F440"), size = 0.9)+
@@ -44,17 +52,11 @@ ggplot(tonga_to_plot)+
   theme_minimal()+
   facet_wrap( .~ Station, scales = "free_x")
 
-ggplot(tonga_to_plot)+
-  geom_point(aes( x = ratio440_470, y = -Pressure, colour = "440/470"), size = 1.5)+
-  geom_point(aes( x = ratio440_532, y = -Pressure, colour = "440/532"), size = 1.5)+
-  scale_color_brewer(palette = "YlGnBu")+
-  ylim(-1000, -500)+
-  xlim(0.85,1.2)+
-  theme_minimal()
 
-tonga_to_plot$cast <- substr(tonga_to_plot$Station, 14,16)
+tonga_clean <- left_join(tonga_to_plot, select(tonga_to_plot_base, - Pressure), by = c("Station", "bin"))
+tonga_clean$cast <- substr(tonga_clean$Station, 14,16) #create a column to know if the profile is up or down
 
-ggplot(filter(tonga_to_plot, cast != "up"))+
+ggplot(filter(tonga_clean, cast != "up"))+
   geom_path(aes(x = F532, y = -Pressure, colour = "F532"), size = 0.9)+
   geom_path(aes(x = F440, y = -Pressure, colour = "F440"), size = 0.9)+
   geom_path(aes(x = F470, y = -Pressure, colour = "F470"), size = 0.9)+
@@ -62,3 +64,17 @@ ggplot(filter(tonga_to_plot, cast != "up"))+
   ylim(-500,0)+
   theme_minimal()+
   facet_wrap( .~ Station, scales = "free_x", ncol = 4)
+
+
+ggplot(tonga_clean)+
+  geom_path(aes(x = FCHL, y = -Pressure, colour = "FCHL"), size = 0.9)+
+  geom_path(aes(x = F440, y = -Pressure, colour = "F440"), size = 0.9)+
+  scale_color_brewer(palette = "Set1")+
+  ylim(-500,0)+
+  theme_minimal()+
+  facet_wrap( .~ Station, scales = "free_x", ncol = 4)
+
+
+
+
+
