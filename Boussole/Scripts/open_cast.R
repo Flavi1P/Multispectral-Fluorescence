5b779ac2ff6d99d69fbce5fd9a9e8ce7a385aa5d
Flavi1P <flavien.petit@obs-vlfr.fr>
2021-01-29 15:12:24 +0100

    write rade desc 2 and plot

--- Boussole/Scripts/open_cast.R
+++ Boussole/Scripts/open_cast.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 library(lubridate)
 library(zoo)
+library(patchwork)
 
-first <- readLines("Boussole/Data/SBEMOOSE/Work/cnv/Test1.cnv")
+first <- readLines("Boussole/Data/SBEMOOSE/Work/cnv/test02.cnv")
 start_line <- grep('END', first)
 
-ctd <- read_table2("Boussole/Data/SBEMOOSE/Work/cnv/Test1.cnv", 
+ctd <- read_table2("Boussole/Data/SBEMOOSE/Work/cnv/test02.cnv", 
                           col_names = FALSE, skip = start_line)
 
 names(ctd) <- c('pres', 'time', 'temp', 'conductivity',
@@ -17,14 +18,14 @@ ctd_clean <- ctd[1:min(which(ctd$pres == max(ctd$pres))),] #for desc
 
 
 
-origin <- 9*3600 + 0* 60 + 13
+origin <- 10*3600 + 30* 60 + 46
 ctd_clean$time <- seconds_to_period(origin + ctd_clean$time)
 
 ggplot(ctd_clean)+
   geom_path(aes(x = fluo_chl, y = -pres))
 
-echo <- read_table2("Boussole/Data/raw/log_rad_1.txt", 
-                              col_names = FALSE, skip = 3)
+echo <- read_table2("Boussole/Data/raw/log_rad_2.txt", 
+                              col_names = FALSE, skip = 2)
 
 
 echo$X4 <- hms(echo$X4)
@@ -71,14 +72,46 @@ multiplex <- multiplex[which(!is.na(multiplex$time)),]
 
 full_df <- bind_rows(ctd_clean, multiplex)
 
-full_df$time <- as.POSIXct(full_df$time, origin = '2020-09-12', tz = 'UTC')
+full_df$time <- as.POSIXct(full_df$time, origin = '2021-28-01', tz = 'UTC')
 full_df$time <- format(full_df$time, '%H:%M:%S')
 
-ggplot(filter(full_df, fluo_440 < 200))+
-  geom_point(aes(x = chl_hi_gain_counts, y = -pres))
-
-write_csv(full_df, 'Boussole/Output/Data/rad_desc_1_28012021.csv')
+write_csv(full_df, 'Boussole/Output/Data/rad_desc_2_28012021.csv')
 
 
+full_df %>% select(pres, fluo_440, fluo_470, fluo_532) %>% 
+  pivot_longer(2:4, names_to = 'wl', values_to = 'counts') %>% ggplot()+
+  geom_point(aes(x = counts, y = -pres, colour = wl), alpha = 0.7)+
+  xlim(25,200)+
+  ggtitle('3X1M fluo')
+
+ggsave('Boussole/Output/Plots/3x1m_rade.png')
 
 
+flbb <- full_df %>% select(pres, fluo_flbb, bb700, cdom) %>%
+  pivot_longer(2:4, names_to = 'variable', values_to = 'val')
+flbb <- flbb[max(which(flbb$pres < 2)):nrow(flbb),]
+
+ggplot(filter(flbb, variable != 'bb700'))+
+  geom_point(aes(x = val, y = -pres, colour = variable), alpha = 0.7)+
+  xlim(0,100)+
+  ggtitle('flbb cdom + fluo')+
+ggplot(filter(flbb,variable == 'bb700', val < 20000))+
+  geom_point(aes(x = val , y = -pres, colour = 'bb700'))+
+  scale_color_viridis_d()+
+  ggtitle('flbb bb700')
+
+ggsave('Boussole/Output/Plots/flbb_rade.png')
+
+
+ecov2 <- full_df %>% select(pres, chl_hi_gain_counts, chl2_hi_gain_counts, beta_700_hi_gain_counts, fdom_hi_gain_counts) %>% 
+  pivot_longer(2:5, names_to = 'variable', values_to = 'val')
+
+ecov2 <- ecov2[max(which(ecov2$pres < 2)):nrow(ecov2),]
+ggplot(filter(ecov2, variable %in% c('chl_hi_gain_counts', 'chl2_hi_gain_counts')))+
+  geom_point(aes(x = val, y = - pres, colour = variable))+
+  ggtitle('ECOV2 fluo profiles')+
+ggplot(filter(ecov2, !variable %in% c('chl_hi_gain_counts', 'chl2_hi_gain_counts')))+
+  geom_point(aes(x = val, y = - pres, colour = variable))+
+  ggtitle('ECOV2 bb700 + fdom profiles')
+
+ggsave('Boussole/Output/Plots/ecov2_rade.png')
