4869999680645a6237d8440f5e9d6c506a282ee7
Flavi1P <flavien.petit@obs-vlfr.fr>
2021-02-23 16:00:48 +0100

    write b225

--- Boussole/Scripts/open_cast.R
+++ Boussole/Scripts/open_cast.R
@@ -4,7 +4,7 @@ library(zoo)
 library(patchwork)
 library(stringr)
 
-first <- readLines("Boussole/Data/SBEMOOSE/Work/cnv/bous227_02.cnv")
+first <- readLines("Boussole/Data/SBEMOOSE/Work/cnv/bous225_03.cnv")
 start_line <- grep('END', first)
 
 hour_line <- first[grep('start_time', first)]
@@ -15,14 +15,14 @@ mn <- as.numeric(substr(start_time, 4,5))
 sc <- as.numeric(substr(start_time, 7,8))
 
 
-ctd <- read_table2("Boussole/Data/SBEMOOSE/Work/cnv/bous227_02.cnv", 
+ctd <- read_table2("Boussole/Data/SBEMOOSE/Work/cnv/bous225_03.cnv", 
                           col_names = FALSE, skip = start_line)
 
 names(ctd) <- c('pres', 'time', 'temp', 'conductivity',
                 'sbeox0v', 'fluo_chl', 'upoly0', 'upoly1', 'potemp',
                 'sal00', 'sigma', 'sbeox_ml', 'sbeox_mm', 'flag')
 
-#ctd_clean <- ctd[min(which(ctd$pres == max(ctd$pres))):max(which(ctd$pres == min(ctd$pres))),] #for asc
+#ctd_clean <- ctd[min(which(ctd$pres == max(ctd$pres))):min(which(ctd$pres == min(ctd$pres))),] #for asc
 ctd_clean <- ctd[1:min(which(ctd$pres == max(ctd$pres))),] #for desc
 
 
@@ -30,13 +30,13 @@ ctd_clean <- ctd[1:min(which(ctd$pres == max(ctd$pres))),] #for desc
 origin <- hr*3600 + mn* 60 + sc
 ctd_clean$time <- seconds_to_period(origin + ctd_clean$time)
 
-ctd_clean <- ctd_clean[1:min(which(ctd_clean$pres < 2)),]
+#ctd_clean <- ctd_clean[1:min(which(ctd_clean$pres < 2)),]
 
 ggplot(ctd_clean)+
   geom_path(aes(x = fluo_chl, y = -pres))+
   xlim(0,1.5)
-echo <- read_table2("Boussole/Data/raw/B227_cast1.txt", 
-                              col_names = FALSE, skip = 2)
+echo <- read_table2("Boussole/Data/raw/bouss225_03.txt", 
+                              col_names = FALSE, skip = 3)
 
 
 echo$X4 <- hms(echo$X4)
@@ -63,8 +63,8 @@ echo_3x1m <- select(echo_3x1m, -X6, -X7, -X8, -X10, -X12, - c(X15:X24))
 flbb <- select(flbb, -X6, -X7, -X8,-X10, -X12, - c(X15:X24))
 
 
-names(echo_3x1m) <- c('month', 'day_name', 'day', 'time', 'year', 'fluo_440', 'fluo_470', 'fluo_532', 'echo3x', 'second', 'pres')
-names(flbb) <- c('month', 'day_name', 'day', 'time', 'year', 'fluo_flbb', 'bb700', 'cdom', 'echov1', 'second', 'pres')
+names(echo_3x1m) <- c('day_name', 'month', 'day', 'time', 'year', 'fluo_440', 'fluo_470', 'fluo_532', 'echo3x', 'second', 'pres')
+names(flbb) <- c('day_name', 'month', 'day', 'time', 'year', 'fluo_flbb', 'bb700', 'cdom', 'echov1', 'second', 'pres')
 
 ecov2names <- c('FrameSync,Counter,
 CHL HiGain(Counts),CHL LoGain(Counts),CHL LTC(Counts),CHL Raw(Counts),
@@ -86,7 +86,10 @@ full_df <- bind_rows(ctd_clean, multiplex)
 full_df$time <- as.POSIXct(full_df$time, origin = '2021-28-01', tz = 'UTC')
 full_df$time <- format(full_df$time, '%H:%M:%S')
 
-write_csv(full_df, 'Boussole/Output/Data/b227_asc1.csv')
+
+#####attention##################################
+###########################################"
+write_csv(full_df, 'Boussole/Output/Data/b225_asc3.csv')
 
 
 full_df %>% select(pres, fluo_440, fluo_470, fluo_532) %>% 
